e0a9a1cbe62e2d2e7c467cc7e1b393b75831ddc3
imre-gabor <gabor@aut.bme.hu>
2022-01-13 19:47:28 +0100

    querydsl

--- airport/src/main/java/hu/webuni/airport/service/AirportService.java
+++ airport/src/main/java/hu/webuni/airport/service/AirportService.java
@@ -1,18 +1,24 @@
 package hu.webuni.airport.service;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.google.common.collect.Lists;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
+
 import hu.webuni.airport.model.Airport;
 import hu.webuni.airport.model.Flight;
+import hu.webuni.airport.model.QFlight;
 import hu.webuni.airport.repository.AirportRepository;
 import hu.webuni.airport.repository.FlightRepository;
 import lombok.RequiredArgsConstructor;
@@ -74,6 +80,34 @@ public class AirportService {
 		flight.setTakeoffTime(takeoffDateTime);
 		return flightRepository.save(flight);
 	}
+//	
+//	public List<Flight> findFlightsByExample(Flight example){
+//		
+//		long id = example.getId();
+//		String flightNumber = example.getFlightNumber();
+//		String takeoffIata = null;
+//		Airport takeoff = example.getTakeoff();
+//		if(takeoff != null)
+//			takeoffIata  = takeoff.getIata();
+//		LocalDateTime takeoffTime = example.getTakeoffTime();
+//		
+//		Specification<Flight> spec = Specification.where(null);
+//		
+//		if(id > 0) {
+//			spec = spec.and(FlightSpecifications.hasId(id));
+//		}
+//		
+//		if(StringUtils.hasText(flightNumber))
+//			spec = spec.and(FlightSpecifications.hasFlightNumber(flightNumber));
+//		
+//		if(StringUtils.hasText(takeoffIata))
+//			spec = spec.and(FlightSpecifications.hasTakoffIata(takeoffIata));
+//		
+//		if(takeoffTime != null)
+//			spec = spec.and(FlightSpecifications.hasTakoffTime(takeoffTime));
+//		
+//		return flightRepository.findAll(spec, Sort.by("id"));
+//	}
 	
 	public List<Flight> findFlightsByExample(Flight example){
 		
@@ -85,21 +119,22 @@ public class AirportService {
 			takeoffIata  = takeoff.getIata();
 		LocalDateTime takeoffTime = example.getTakeoffTime();
 		
-		Specification<Flight> spec = Specification.where(null);
+		List<Predicate> predicates = new ArrayList<>();
+		QFlight flight = QFlight.flight;
 		
-		if(id > 0) {
-			spec = spec.and(FlightSpecifications.hasId(id));
-		}
+		if(id > 0)
+			predicates.add(flight.id.eq(id));
 		
 		if(StringUtils.hasText(flightNumber))
-			spec = spec.and(FlightSpecifications.hasFlightNumber(flightNumber));
+			predicates.add(flight.flightNumber.startsWithIgnoreCase(flightNumber));
 		
 		if(StringUtils.hasText(takeoffIata))
-			spec = spec.and(FlightSpecifications.hasTakoffIata(takeoffIata));
+			predicates.add(flight.takeoff.iata.startsWith(takeoffIata));
 		
-		if(takeoffTime != null)
-			spec = spec.and(FlightSpecifications.hasTakoffTime(takeoffTime));
-		
-		return flightRepository.findAll(spec, Sort.by("id"));
+		if(takeoffTime != null) {
+			LocalDateTime startOfDay = LocalDateTime.of(takeoffTime.toLocalDate(), LocalTime.MIDNIGHT);
+			predicates.add(flight.takeoffTime.between(startOfDay, startOfDay.plusDays(1)));
+		}
+		return Lists.newArrayList(flightRepository.findAll(ExpressionUtils.allOf(predicates), Sort.by("id")));
 	}
 }
