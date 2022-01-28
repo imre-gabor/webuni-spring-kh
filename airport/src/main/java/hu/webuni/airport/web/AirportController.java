d42173f455e4b2b8a7cd224c85dc448ccd851fee
imre-gabor <gabor@aut.bme.hu>
2022-01-28 10:19:43 +0100

    implement historical data storage with Envers

--- airport/src/main/java/hu/webuni/airport/web/AirportController.java
+++ airport/src/main/java/hu/webuni/airport/web/AirportController.java
@@ -1,5 +1,6 @@
 package hu.webuni.airport.web;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -24,6 +25,7 @@ import org.springframework.web.server.ResponseStatusException;
 import hu.webuni.airport.dto.AirportDto;
 import hu.webuni.airport.mapper.AirportMapper;
 import hu.webuni.airport.model.Airport;
+import hu.webuni.airport.model.HistoryData;
 import hu.webuni.airport.repository.AirportRepository;
 import hu.webuni.airport.service.AirportService;
 import lombok.RequiredArgsConstructor;
@@ -59,6 +61,24 @@ public class AirportController {
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
 		return airportMapper.airportSummaryToDto(airport);
+	}
+	
+	@GetMapping("/{id}/history")
+	public List<HistoryData<AirportDto>> getHistoryById(@PathVariable long id) {
+		List<HistoryData<Airport>> airports = airportService.getAirportHistory(id);
+		
+		List<HistoryData<AirportDto>> airportDtosWithHistory = new ArrayList<>();
+		
+		airports.forEach(hd ->{
+			airportDtosWithHistory.add(new HistoryData<>(
+					airportMapper.airportToDto(hd.getData()),
+					hd.getRevType(),
+					hd.getRevision(),
+					hd.getDate()
+					));
+		});
+		
+		return airportDtosWithHistory;
 	}
 	
 	@PostMapping
