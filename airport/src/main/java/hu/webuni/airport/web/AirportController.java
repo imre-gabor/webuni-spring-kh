3b72fa7996e4e09bbec139306734fc2801732d83
imre-gabor <gabor@aut.bme.hu>
2022-02-03 01:54:46 +0100

    upload and download binary data

--- airport/src/main/java/hu/webuni/airport/web/AirportController.java
+++ airport/src/main/java/hu/webuni/airport/web/AirportController.java
@@ -1,5 +1,6 @@
 package hu.webuni.airport.web;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import hu.webuni.airport.api.AirportControllerApi;
@@ -27,6 +29,7 @@ import hu.webuni.airport.mapper.AirportMapper;
 import hu.webuni.airport.mapper.HistoryDataMapper;
 import hu.webuni.airport.model.Airport;
 import hu.webuni.airport.model.HistoryData;
+import hu.webuni.airport.model.Image;
 import hu.webuni.airport.repository.AirportRepository;
 import hu.webuni.airport.service.AirportService;
 import lombok.RequiredArgsConstructor;
@@ -131,5 +134,20 @@ public class AirportController implements AirportControllerApi {
 		return pageable;
 	}
 
+	@Override
+	public ResponseEntity<String> uploadImageForAirport(Long id, @Valid String fileName,
+			@Valid MultipartFile content) {
+		Image image;
+		try {
+			image = airportService.saveImageForAirport(id, fileName, content.getBytes());
+			return ResponseEntity.ok("/api/images/" + image.getId());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	
+	
 	
 }
