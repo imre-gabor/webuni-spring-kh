e064bd665d51b099a05d782638f02b6ba252e050
imre-gabor <gabor@aut.bme.hu>
2022-02-08 14:20:47 +0100

    add async demo

--- airport/src/main/java/hu/webuni/airport/service/DelayService.java
+++ airport/src/main/java/hu/webuni/airport/service/DelayService.java
@@ -10,12 +10,12 @@ public class DelayService {
 	private Random random = new Random();
 	
 	public int getDelay(long flightId) {
-		System.out.println("getDelay called");
+		System.out.println("DelayService.getDelay called at thread " + Thread.currentThread().getName());
 
-//		try {
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e) {
-//		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
 		return random.nextInt(0, 1800);
 	}
 }
