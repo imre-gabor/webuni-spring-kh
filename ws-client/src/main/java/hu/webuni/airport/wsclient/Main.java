5376487ea09c998cd5711d400e23f308aa42dc6a
imre-gabor <gabor@aut.bme.hu>
2022-02-22 15:02:45 +0100

    add asyc client call

--- ws-client/src/main/java/hu/webuni/airport/wsclient/Main.java
+++ ws-client/src/main/java/hu/webuni/airport/wsclient/Main.java
@@ -1,8 +1,10 @@
 package hu.webuni.airport.wsclient;
 
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		AirportXmlWs airportXmlWsImplPort = new AirportXmlWsImplService()
 			.getAirportXmlWsImplPort();
@@ -12,10 +14,26 @@ public class Main {
 			.forEach(h ->{
 				System.out.format("Name:%s, Revision:%d, RevType:%s%n", h.getData().getName(), h.getRevision(), h.getRevType().toString());
 			});
+
+		
 		
 		GetFlightDelay flightDelay = new GetFlightDelay();
 		flightDelay.setArg0(123L);
-		System.out.println(airportXmlWsImplPort.getFlightDelay(flightDelay));
+		
+		
+//		System.out.println(airportXmlWsImplPort.getFlightDelay(flightDelay));
+		airportXmlWsImplPort.getFlightDelayAsync(flightDelay, res -> {
+			try {
+				System.out.println(res.get());
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		});
+	
+		for(int i=0;i<6; i++) {
+			System.out.println("Client doing something else...");
+			Thread.sleep(1000);
+		}
 	}
 
 }
