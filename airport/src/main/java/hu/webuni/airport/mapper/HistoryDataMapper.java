e7ee0607dc9d4bb83cfab4255952b8da0443b6d2
imre-gabor <gabor@aut.bme.hu>
2022-02-22 20:56:28 +0100

    add synchronous and asyncronous web services with CXF

--- airport/src/main/java/hu/webuni/airport/mapper/HistoryDataMapper.java
+++ airport/src/main/java/hu/webuni/airport/mapper/HistoryDataMapper.java
@@ -1,9 +1,12 @@
 package hu.webuni.airport.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import hu.webuni.airport.api.model.FlightDto;
 import hu.webuni.airport.api.model.HistoryDataAirportDto;
 import hu.webuni.airport.model.Airport;
+import hu.webuni.airport.model.Flight;
 import hu.webuni.airport.model.HistoryData;
 
 @Mapper(componentModel = "spring")
@@ -11,5 +14,7 @@ public interface HistoryDataMapper {
 
 	HistoryDataAirportDto airportHistoryDataToDto(HistoryData<Airport> hd);
 
-	
+	@Mapping(ignore = true, target = "takeoff")
+	@Mapping(ignore = true, target = "landing")
+	FlightDto flightToDto(Flight flight);
 }
--- airport/src/main/java/hu/webuni/airport/service/DelayService.java
+++ airport/src/main/java/hu/webuni/airport/service/DelayService.java
@@ -1,7 +1,9 @@
 package hu.webuni.airport.service;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +19,16 @@ public class DelayService {
 		} catch (InterruptedException e) {
 		}
 		return random.nextInt(0, 1800);
+	}
+	
+	@Async
+	public CompletableFuture<Integer> getDelayAsync(long flightId) {
+		System.out.println("DelayService.getDelay called at thread " + Thread.currentThread().getName());
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		return CompletableFuture.completedFuture(random.nextInt(0, 1800));
 	}
 }
