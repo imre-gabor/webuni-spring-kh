98e92a56da82156b4602a55ff82d74ca6d768ab0
imre-gabor <gabor@aut.bme.hu>
2022-02-22 14:50:02 +0100

    add async WS operation

--- airport/src/main/java/hu/webuni/airport/service/DelayService.java
+++ airport/src/main/java/hu/webuni/airport/service/DelayService.java
@@ -1,7 +1,9 @@
 package hu.webuni.airport.service;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,4 +20,16 @@ public class DelayService {
 		}
 		return random.nextInt(0, 1800);
 	}
+
+	@Async
+	public CompletableFuture<Integer> getDelayAsync(long flightId) {
+		System.out.println("DelayService.getDelay called at thread " + Thread.currentThread().getName());
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		return CompletableFuture.completedFuture(random.nextInt(0, 1800));
+	}
+
 }
--- ws-client/src/main/java/hu/webuni/airport/wsclient/Main.java
+++ ws-client/src/main/java/hu/webuni/airport/wsclient/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		new AirportXmlWsImplService()
-			.getAirportXmlWsImplPort()
+		AirportXmlWs airportXmlWsImplPort = new AirportXmlWsImplService()
+			.getAirportXmlWsImplPort();
+		
+		airportXmlWsImplPort
 			.getHistoryById(462L)
 			.forEach(h ->{
 				System.out.format("Name:%s, Revision:%d, RevType:%s%n", h.getData().getName(), h.getRevision(), h.getRevType().toString());
 			});
+		
+		GetFlightDelay flightDelay = new GetFlightDelay();
+		flightDelay.setArg0(123L);
+		System.out.println(airportXmlWsImplPort.getFlightDelay(flightDelay));
 	}
 
 }
