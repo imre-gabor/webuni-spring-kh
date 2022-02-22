b59c943cbbc3ab463ce58cff4a02a7e1351ed3c9
imre-gabor <gabor@aut.bme.hu>
2022-02-22 18:04:48 +0100

    add JMS consumer application

---
+++ customer-service/src/main/java/hu/webuni/airport/customer/CustomerServiceApplication.java
@@ -0,0 +1,49 @@
+package hu.webuni.airport.customer;
+
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setObjectMapper(objectMapper);
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// This provides all boot's default to this factory, including the message
+		// converter
+		((SingleConnectionFactory)connectionFactory).setClientId("customer-service");
+		configurer.configure(factory, connectionFactory);
+		// You could still override some of Boot's default if necessary.
+//		factory.setConcurrency("1");
+		factory.setSubscriptionDurable(true);
+//		factory.setClientId("customer-service");
+		return factory;
+	}
+}
