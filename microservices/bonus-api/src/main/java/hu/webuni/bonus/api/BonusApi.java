7ab9efff3ae43bb5a35a79b2591043c7d4e0a5bc
imre-gabor <gabor@aut.bme.hu>
2022-03-15 17:54:51 +0100

    add feign client and use it at booking app

--- microservices/bonus-api/src/main/java/hu/webuni/bonus/api/BonusApi.java
+++ microservices/bonus-api/src/main/java/hu/webuni/bonus/api/BonusApi.java
@@ -1,10 +1,12 @@
 package hu.webuni.bonus.api;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name="bonus", url = "${feign.bonus.url}")
 public interface BonusApi {
 
     @GetMapping("/bonus/{user}")
--- microservices/booking/src/main/java/hu/webuni/booking/web/BookingController.java
+++ microservices/booking/src/main/java/hu/webuni/booking/web/BookingController.java
@@ -1,13 +1,17 @@
 package hu.webuni.booking.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.webuni.bonus.api.BonusApi;
 import hu.webuni.booking.dto.PurchaseData;
 import hu.webuni.booking.dto.TicketData;
+import hu.webuni.currency.api.CurrencyApi;
+import hu.webuni.flights.api.FlightsApi;
 
 @RestController
 @RequestMapping("/api")
@@ -15,9 +19,21 @@ public class BookingController {
 
     @Value("${booking.bonus}")
     double bonusRate;
+    
+    @Autowired
+    BonusApi bonusApi;
+    @Autowired
+    CurrencyApi currencyApi;
+    @Autowired
+    FlightsApi flightsApi;
 
     @PostMapping("/ticket")
     public PurchaseData buyTicket(@RequestBody TicketData ticketData) {
+    	
+    	System.out.println(bonusApi.getPoints(ticketData.getUser()));
+    	System.out.println(currencyApi.getRate("USD", "HUF"));
+    	System.out.println(flightsApi.searchFlight(ticketData.getFrom(), ticketData.getTo()));
+    	
         return null;
     }
 }
