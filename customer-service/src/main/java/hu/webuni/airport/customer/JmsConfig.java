d3fc8765e7229b857421886b3fcc512910684423
imre-gabor <gabor@aut.bme.hu>
2023-07-09 11:42:28 +0200

    set durable subscription

--- customer-service/src/main/java/hu/webuni/airport/customer/JmsConfig.java
+++ customer-service/src/main/java/hu/webuni/airport/customer/JmsConfig.java
@@ -1,12 +1,18 @@
 package hu.webuni.airport.customer;
 
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.jms.ConnectionFactory;
 
 @Configuration
 public class JmsConfig {
@@ -20,7 +26,7 @@ public class JmsConfig {
 		return converter;
 	}
 	
-	/*
+	
 	@Bean
 	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer){
 		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
@@ -30,6 +36,6 @@ public class JmsConfig {
 		factory.setSubscriptionDurable(true);
 //		factory.setClientId("cusomter-service");
 		return factory;
-	}*/
+	}
 	
 }
